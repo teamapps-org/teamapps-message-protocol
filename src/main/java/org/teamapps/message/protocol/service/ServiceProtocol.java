a57c205fcbb4c615158548c01c0ab76bb298ad7e
Matthias Bernstein <matthias.bernstein@teamapps.org>
2023-02-02 23:01:12 +0100

    updated header files

--- src/main/java/org/teamapps/message/protocol/service/ServiceProtocol.java
+++ src/main/java/org/teamapps/message/protocol/service/ServiceProtocol.java
@@ -1,8 +1,8 @@
 /*-
  * ========================LICENSE_START=================================
- * TeamApps Protocol Schema
+ * TeamApps Message Protocol
  * ---
- * Copyright (C) 2022 TeamApps.org
+ * Copyright (C) 2022 - 2023 TeamApps.org
  * ---
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
